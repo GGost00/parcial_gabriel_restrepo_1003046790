3e265a48c17216d35d718a76457b7dbd69c6ba23
gabriel <Gabriel.restrepoi@gmail.com>
2020-10-17 16:28:37 -0500

    termine funcion peligro la cual retorna true si el cañon defencivo esta en peligro de daño

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 
 using namespace std;
-bool peligro();
+bool peligro(int x,int y);
 int main()
 {
     int pos_canon_ofensiva[2]={};
@@ -15,17 +15,34 @@ int main()
     int distancia;
     distancia=pos_canon_defenciva[0]-pos_canon_ofensiva[0];
     cout<<distancia<<endl;
-    peligro();
+    if(peligro(pos_canon_defenciva[0],pos_canon_defenciva[1])==true){
+
+    }
+
     return 0;
 }
-bool peligro(){
+bool peligro(int x,int y){
     int bala_velocidad_angulo[2]={},vx_bala,vy_bala;
+    float t=0;
 
     cout<<"ingrese la velocidad de la bala ";
     cin>>bala_velocidad_angulo[0];cout<<"ingrese el angulo de la bala ";
     vx_bala=bala_velocidad_angulo[0]* cos(bala_velocidad_angulo[1]);
     vy_bala=bala_velocidad_angulo[0]* sin(bala_velocidad_angulo[1]);
     int pos_x_bala, pos_y_bala;
-    pos_x_bala=
+    while(true){
+        pos_x_bala=x+(vx_bala*t);
+        pos_y_bala=y+(vy_bala*t)-(1/2*(9.81)*(t*t));
+        t+=0.5 ;
+        if((((pos_x_bala-x)^2+(pos_y_bala-y)^2)^(1/2))<=5){
+            cout<<"el cañon esta en peligro!";
+            return true;
+        }
+        if(t==100.0){
+            cout<<"no hay peligro";
+            return false;
+        }
+
+      }
 
 }
