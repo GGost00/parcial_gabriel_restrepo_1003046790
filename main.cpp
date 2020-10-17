33c5d29e626f2406bd96f49edee1e4af1d45e023
gabriel <Gabriel.restrepoi@gmail.com>
2020-10-17 16:52:38 -0500

    tratando de solucionar porblemas en el codigo

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 #include <math.h>
 
 using namespace std;
-bool peligro(int x,int y);
+bool peligro(int x,int y,float*);
 int main()
 {
     int pos_canon_ofensiva[2]={};
     int pos_canon_defenciva[2]={};
+    float t=2.5,vx_bala,vy_bala,angulo;
+    float *p=&vx_bala;
+
+
     cout<<"ingrese la pos x del canon ofencivo ";
     cin>>pos_canon_ofensiva[0];cout<<"ingrese la pos y del canon ofencivo ";
     cin>>pos_canon_ofensiva[1];cout<<"ingrese la pos x del canon defencivo ";
@@ -15,27 +19,32 @@ int main()
     int distancia;
     distancia=pos_canon_defenciva[0]-pos_canon_ofensiva[0];
     cout<<distancia<<endl;
-    if(peligro(pos_canon_defenciva[0],pos_canon_defenciva[1])==true){
+    if(peligro(pos_canon_ofensiva[0],pos_canon_ofensiva[1],p)==true){
 
     }
 
     return 0;
 }
-bool peligro(int x,int y){
-    int bala_velocidad_angulo[2]={},vx_bala,vy_bala;
-    float t=0;
+bool peligro(int x,int y,float *p){
+    int bala_velocidad_angulo[2]={};
+    float t=0,vx_bala,vy_bala;
 
     cout<<"ingrese la velocidad de la bala ";
     cin>>bala_velocidad_angulo[0];cout<<"ingrese el angulo de la bala ";
+    cin>>bala_velocidad_angulo[1];
     vx_bala=bala_velocidad_angulo[0]* cos(bala_velocidad_angulo[1]);
     vy_bala=bala_velocidad_angulo[0]* sin(bala_velocidad_angulo[1]);
     int pos_x_bala, pos_y_bala;
+    *p=vx_bala;
+    p+=1;
+    *p=vy_bala;
+
     while(true){
         pos_x_bala=x+(vx_bala*t);
         pos_y_bala=y+(vy_bala*t)-(1/2*(9.81)*(t*t));
-        t+=0.5 ;
-        if((((pos_x_bala-x)^2+(pos_y_bala-y)^2)^(1/2))<=5){
-            cout<<"el cañon esta en peligro!";
+        t+=0.2 ;
+        if(0<(((pos_x_bala-x)^2+(pos_y_bala-y)^2)^(1/2))&&(((pos_x_bala-x)^2+(pos_y_bala-y)^2)^(1/2))<=5){
+            cout<<"el canon esta en peligro!";
             return true;
         }
         if(t==100.0){
